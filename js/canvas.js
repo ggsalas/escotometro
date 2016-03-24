93599266572740ff52a65789e4107ccdd308f5b7
Gabriel Salas <ggsalas@gmail.com>
2016-03-24 17:47:43 -0300

    system working ok

--- js/canvas.js
+++ js/canvas.js
@@ -1,24 +1,28 @@
-// for development
+// development
+// $(function(){ drawSide(); });
+
+//* Drawing styles
 var widthHeavy = 10;
 var widthThin = 1;
 var colorDark = "#000";
 var colorLight = "#666";
 
-function drawTop() {
+function drawTop(start, stop) {
   var width = 360;
   var height = 400;
   var centerX = 180;
   var centerY = 220;
-  var solarpathStart = Math.PI/4;
-  var solarpathEnd = Math.PI/4*3;
+  var solarpathStart = start; // Math.PI/4;
+  var solarpathEnd = stop; //Math.PI/4*3;
 
   var canvas = document.getElementById('canvas-top');
   if (canvas.getContext){
     var ctx = canvas.getContext('2d');
+    // update on each modification 
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     
     //* Light and thin
     ctx.beginPath();
-    
     ctx.arc(centerX,centerY,160,0,Math.PI*2,true); // circle
     
     ctx.moveTo(0, centerY); // x
@@ -44,6 +48,49 @@ function drawTop() {
     ctx.beginPath();
     ctx.arc(centerX, centerY, 160, solarpathStart, solarpathEnd,true); // circle
 
+    ctx.lineWidth = widthHeavy;
+    ctx.strokeStyle= colorDark;    
+    ctx.stroke();
+  }
+}
+
+function drawSide(angle) {
+  var width = 360;
+  var height = 400;
+  var centerX = 180;
+  var centerY = 220;
+  var arcCenterY = height - 25;
+
+  var canvas = document.getElementById('canvas-side');
+  if (canvas.getContext){
+    var ctx = canvas.getContext('2d');
+    // update on each modification 
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    //* Light and heavy
+    ctx.beginPath();    
+    ctx.moveTo(0, arcCenterY);
+    ctx.lineTo(width, arcCenterY); // North mark
+
+    ctx.lineWidth = widthHeavy;
+    ctx.strokeStyle = colorLight;
+    ctx.stroke();  
+
+    //* Light and thin
+    ctx.beginPath();
+    ctx.arc(20, arcCenterY, 315, Math.PI, Math.PI*2); // arc
+
+    ctx.moveTo(20, 0); // y
+    ctx.lineTo(20, height);
+
+    ctx.lineWidth = widthThin;
+    ctx.strokeStyle = colorLight;
+    ctx.stroke();    
+
+    //* Dark and heavy
+    ctx.beginPath();
+    ctx.arc(20, arcCenterY, 315, Math.PI*2 - angle , Math.PI*2); // arc
+    
     ctx.lineWidth = widthHeavy;
     ctx.strokeStyle= colorDark;    
     ctx.stroke();
--- js/escotometro.js
+++ js/escotometro.js
@@ -31,27 +31,57 @@ var result = function (date, lat ) {
   var times = SunCalc.getTimes(date, lat, lon);
 
   // Get sunrise azimuth in degrees
-  var sunriseAzimuth = SunCalc.getPosition(times.sunrise,lat, lon).azimuth * 180 / Math.PI; 
+  var sunriseAzimuth = SunCalc.getPosition(times.sunrise,lat, lon).azimuth; // * 180 / Math.PI; 
 
   // Get sunset azimuth in degrees
-  var sunsetAzimuth = SunCalc.getPosition(times.sunset,lat, lon).azimuth * 180 / Math.PI;
+  var sunsetAzimuth = SunCalc.getPosition(times.sunset,lat, lon).azimuth; // * 180 / Math.PI;
 
   // Get max altitude above horizon on the day
-  var maxAltitude = SunCalc.getPosition(times.solarNoon, lat, lon).altitude * 180 / Math.PI;
+  var maxAltitude = SunCalc.getPosition(times.solarNoon, lat, lon).altitude;
+
+  // Get day duration 
+  function msToTime(duration) {
+    var milliseconds = parseInt((duration%1000)/100)
+        , seconds = parseInt((duration/1000)%60)
+        , minutes = parseInt((duration/(1000*60))%60)
+        , hours = parseInt((duration/(1000*60*60))%24);
+
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+    return hours + ":" + minutes;
+  }
+  var dayDuration = msToTime(times.sunset - times.sunrise);
 
   return {
     sunriseAzimuth: sunriseAzimuth,
     sunsetAzimuth: sunsetAzimuth,
     maxAltitude: maxAltitude,
+    dayDuration: dayDuration,
   }
 }
 
 //* Display result
 var updateView = function (position) {
-  $('.sunriseAzimuth').html('<p>Acimut amanecer: ' + position.sunriseAzimuth + '<p>');
+  var start = position.sunriseAzimuth + Math.PI / 2;
+  var stop = position.sunsetAzimuth + Math.PI / 2; 
+  var totalPath = ( stop - start ) * 180 / Math.PI;
+
+  if(latitude <= 0){
+    drawTop(start, stop);
+    totalPath = 360 - (( stop - start ) * 180 / Math.PI);
+  }else{
+    drawTop(stop, start);
+    totalPath = ( stop - start ) * 180 / Math.PI;
+  }
+  drawSide(position.maxAltitude);
+
+  $('.sunriseAzimuth').html('<p>Acimut amanecer: ' + position.sunriseAzimuth +
+                            'recorrido total: ' + totalPath +
+                            'duración del día: ' + position.dayDuration + '<p>');
   $('.sunsetAzimuth').html('<p>Acimut anochecer: ' + position.sunsetAzimuth + '<p>');
-  $('.maxAltitude').html('<p>Altura máxima sobre el horizonte: ' + position.maxAltitude + '<p>');
-  drawTop();
+  $('.maxAltitude').html('<p>Altura máxima sobre el horizonte: ' + position.maxAltitude * 180 / Math.PI + '<p>'); 
 }
 
 // Change solstice name for northern hemisphere
